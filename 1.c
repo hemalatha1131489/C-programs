25bce0ea842ae0e050065ecde185d99f49d729fd
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-01-08 16:50:37 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int number;
+	printf("\nEnter your number");
+	scanf("%d",&number);
+	if(number%2==0)
+	
+		printf("\nIt is a even number");
+	
+	else
+	printf("\nIt is a odd number");
+
+return 0;
+}
