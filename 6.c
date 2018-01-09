a52ff5ee21f9ca5ff886d233d5cb6faaf4079e00
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-01-09 13:06:15 +0000

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,14 @@
+
+/* leap year or not*/
+#include<stdio.h>
+int main()
+{
+int year;
+printf("\nEnter the year");
+scanf("%d",&year);
+if((year%4==0)&&(year %100==0)&&(year%400==0))
+printf("%d \nis a Leap year",year);
+else
+printf(("\nIt is not a leap year");
+return 0;
+}
