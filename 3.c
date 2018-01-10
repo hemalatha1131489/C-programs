39f45dc632138d1ceb7890e85f8f8d7f2a41eb89
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-01-10 15:47:31 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -5,20 +5,20 @@ int main()
 char chr;
 scanf("%c",&chr);
 printf("Enter the character");
-if((chr>='a'&& chr>='z') || (chr>='A'&& chr>='Z'))
+if((chr>='a'&& chr<='z') || (chr>='A'&& chr<='Z'))
 {
 if(chr=='a'||chr=='A'||chr=='e'||chr=='E'||chr=='i'||chr=='I'||chr=='o'||chr=='O'||chr=='u'||chr=='U')
 {
-printf("%c is a vowel",chr);
+printf("%c\n is a vowel\t",chr);
 }
 else
 {
-printf("%c is a consonant",chr);
+printf("%c\n is a consonant\t",chr);
 }
 }
 else
 {
-printf("%c is neither vowel nor a consonant",chr);
+printf("%c is neither vowel nor a consonant\t",chr);
 }
 return 0;
 }
