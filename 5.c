6ce87aa52f1bc3b2df43b1eba8ab9e0cf5780780
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-01-10 16:02:44 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -4,14 +4,14 @@ int main()
 {
 int num1,num2,num3;
 printf("\nEnter the three numbers");
-scanf("%d, %d ,%d",&num1,&num2,&num3);
+scanf("%d%d%d",&num1,&num2,&num3);
 printf("\nThree numbers are \t%d,\t%d,\t%d",num1,num2,num3);
 if((num1>=num2)&& (num1>=num3))
-printf("%d\n is largest among three numbers",num1);
+printf("\n%d\t is largest among three numbers",num1);
 if((num2>=num1)&&(num2>=num3))
-printf("%d \nis the largest among three numbers",num2);
-if(num3>=num1 )&& (num3>=num2))
-printf("%d \nis the largest among three numbers",num3);
+printf("\n%d \tis the largest among three numbers",num2);
+if((num3>=num1 )&& (num3>=num2))
+printf("\t%d \tis the largest among three numbers",num3);
 return 0;
 }
 
