49b5e2238ead57d2e67619076709cd1160ace97e
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-01-19 12:30:21 +0000

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,19 @@
+//odd numbers between two intervals
+#include<stdio.h>
+int main()
+{
+int firstnumber,lastnumber,temp=0;
+printf("\nEnter the first number");
+scanf("%d",&firstnumber);
+printf("\nEnter the second number");
+scanf("%d",&lastnumber);
+for(i=firstnumber;i<=lastnumber;i++)
+{
+temp=i%2;
+if(temp!=0)
+{
+printf("\n%d",i);
+}
+}
+return 0;
+}
