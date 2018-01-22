fa6b41b3a035de3106f597268dc7e8e9e5a48259
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-01-22 12:32:41 +0000

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,11 @@
+//find the sum of two numbers
+#include<stdio.h>
+int main()
+{
+int num1,num2,sum=0;
+printf("Enter the two numbers");
+scanf("%d%d",&num1,&num2);
+sum=num1+num2;
+printf("\n%d is the sum of %d,%d",sum,num1,num2);
+return 0;
+}
