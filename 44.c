f45713e033f29b3394983db1dad11e2a90997e42
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-01-22 12:18:25 +0000

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,12 @@
+//check number is in the range
+#include<stdio.h>
+int main()
+{int num;
+printf("Enter the number");
+scanf("%d",&num);
+if(num<=10)
+printf("\n%d Yes");
+else
+printf("\n%d is not in the range",num);
+return 0;
+}
