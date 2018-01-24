cb0cf85d62af1225d258027eb0128990c608763d
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-01-24 17:00:48 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,45 @@
+//word print
+#include<stdio.h>
+int main()
+{
+int num,ch;
+printf("Enter the number");
+scanf("%d",&num);
+scanf("%d",&ch);
+switch(ch)
+{
+case 1:
+printf("one");
+break;
+case 2:
+printf("Two");
+break;
+case 3:
+printf("Three");
+break;
+case 4:
+printf("Four");
+break;
+case 5:
+printf("Five");
+break;
+case 6:
+printf("Six");
+break;
+case 7:
+printf("Seven");
+break;
+case 8:
+printf("Eight");
+break;
+case 9:
+printf("Nine");
+break;
+case 10:
+printf("Ten");
+break
+}
+return 0;
+}
+
+
