780128a038b8b89093bf72095d571231d6de9bad
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-01-31 15:50:47 +0530

    Create 59.c

---
+++ 59.c
@@ -0,0 +1,23 @@
+//maximum among ten numbers
+#include<stdio.h>
+int main()
+{
+int a[10],max,i,n;
+printf("Enter the total number");
+scanf("%d",&n);
+printf("\nEnter the numbers");
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=0;i<=n;i++)
+{
+if(a[i]>max)
+{
+max=a[i];
+}
+}
+printf("\nMaximum number is %d",max);
+return 0;
+}
