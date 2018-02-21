b627f159619da823a1c3a65fd60417efb26f96b3
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-02-21 16:51:49 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,13 @@
+//check whether a number si multible of 13
+#include<stdio.h>
+int main()
+{
+int num;
+printf("Enter the number");
+scanf("%d",&num);
+if(num%13==0)
+printf("\nYES");
+else
+printf("\nNO");
+return 0;
+}
