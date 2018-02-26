c88d21dfa77c8b28f0e054b0ad35c7969535b43d
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-02-26 16:29:48 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,11 @@
+//print the area 
+#include<stdio.h>
+int main()
+{
+float l,b,area;//l=length,b=breath
+printf("Enter the parameters");
+scanf("%f %f",&l,&b);
+area=l*b;
+printf("\n%.5f",area);
+return 0;
+}
