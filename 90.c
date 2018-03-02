adecd673a025a58ec6875f6fc44bada58ade0a0b
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-03-02 15:35:53 +0530

    Update 90.c

--- 90.c
+++ 90.c
@@ -3,12 +3,12 @@
 int main()
 {
 char str[20],i;
-printf("Enter the alphanumeric string");
+printf("Enter the alphanumeric string\n");
 scanf("%s",str);
 for(i=0;str[i]!='\0';i++)
 {
-if(str[i]>='0'||str[i]<='9')
-printf("\n%d",str[i]);
+if(str[i]>='0'&&str[i]<='9')
+printf("%c",str[i]);
 }
 return 0;
 }
