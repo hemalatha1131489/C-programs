9d6b2acbb9e1c30097f9de4e272930d5a6e8285a
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-03-02 16:43:08 +0530

    Create 88.c

---
+++ 88.c
@@ -0,0 +1,18 @@
+//lcm of two numbers
+#include<stdio.h>
+int main()
+{
+int n1,n2,x,y;
+printf("Enter the numbers");
+scanf("%d %d",&n1,&n2);
+x=n1,y=n2;
+while(n1!=n2)
+{
+if(n1>n2)
+n1=n1-n2;
+else
+n2=n2-n1;
+}
+printf("\nLcm is %d",x*y/n1);
+return 0;
+}
