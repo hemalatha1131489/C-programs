5d5635135b28ef24324312b88b9855b79b29a2ed
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-03-05 16:55:05 +0530

    Create 93.c

---
+++ 93.c
@@ -0,0 +1,11 @@
+//convert to kelvin
+#include<stdio.h>
+int main()
+{
+int cel,kel=0;
+printf("Enter the celsius value");
+scanf("%d",&cel);
+kel=cel+273;
+printf("\nKelvin value is %d",kel);
+return 0;
+}
