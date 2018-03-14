a4ea7286955c5f763cfbd60e72d7679bf76f4929
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-03-14 16:23:02 +0530

    Create 95.c

---
+++ 95.c
@@ -0,0 +1,11 @@
+//To find interest
+#include<stdio.h>
+int main()
+{
+int p,t,r,inst=0;
+printf("Enter the values of P,T & R respestively");
+scanf("%d%d%d",&p,&t,&r);
+inst=((p*r*t)/100);
+printf("\n%d",inst);
+return 0;
+}
