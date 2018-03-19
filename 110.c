a0712301ef5def8538686779143d68a28792c1d6
hemalatha1131489 <31499216+hemalatha1131489@users.noreply.github.com>
2018-03-19 17:00:20 +0530

    Update 110.c

--- 110.c
+++ 110.c
@@ -2,12 +2,13 @@
 #include<stdio.h>
 int main()
 {
-int i,sum=0,k;
+int i,sum=0,k,a[100];
 printf("Enter the limit");
 for(i=0;i<k;i++)
 {
 scanf("%d",&a[i]);
 sum=sum+a[i];
 }
+  printf("\nsum is %d",sum);
 return 0;
 }
